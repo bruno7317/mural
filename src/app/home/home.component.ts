fe1c59747f12be0ccb66c08c38512de24c1b71d7
Bruno de Souza da Silva <bruno7317@gmail.com>
2024-09-22 16:29:55 -0600

    Reestablishes project navigation.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private router: Router) {}
+
+  goToProject(projectId: string): void {
+    this.router.navigate(['/projects', projectId]);
+  }
+
   projects = [
     {
       id: 'project-1',
