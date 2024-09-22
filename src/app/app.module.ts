7f3b959ab4a4130da66e595208d730471d2101ac
Bruno de Souza da Silva <bruno7317@gmail.com>
2024-09-22 14:01:35 -0600

    Angular update to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,32 +9,25 @@ import { NewEntryComponent } from './components/entries/new-entry/new-entry.comp
 import { ListEntriesComponent } from './components/entries/list-entries/list-entries.component';
 import { FormsModule } from '@angular/forms';
 import { EntryComponent } from './components/entries/entry/entry.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NewEntryComponent,
-    ListEntriesComponent,
-    EntryComponent,
-    AboutComponent,
-    ProjectCardComponent,
-    HomeComponent,
-    ProjectDetailComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        NewEntryComponent,
+        ListEntriesComponent,
+        EntryComponent,
+        AboutComponent,
+        ProjectCardComponent,
+        HomeComponent,
+        ProjectDetailComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
