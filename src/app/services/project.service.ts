0706112a3359eda5f994327df59c2e6582420969
Bruno de Souza da Silva <bruno7317@gmail.com>
2025-01-23 18:21:23 -0700

    Fixes project details

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -35,59 +35,6 @@ export interface Project {
 export class ProjectService {
   private projects: Project[] = [
     {
-      id: 'project-4',
-      title: 'Innovative File Comparison',
-      imageUrl: 'assets/images/tag-safety.png',
-      detailImageUrl: 'assets/images/tag-safety-detail.webp',
-      description:
-        'Developed a CSV file comparison feature for the TagSafety platform, enabling clients to visualize and reconcile updates against existing electrical safety data. Initially unfamiliar with Grails and Groovy, I leveraged AI-assisted learning to quickly adapt, delivering a streamlined, intuitive tool that surpassed the client’s expectations. Enthusiastic feedback prompted additional feature requests, bringing in new revenue opportunities, and earned me personal commendations from the CEO.',
-      objective:
-        'To simplify the client’s workflow for updating electrical safety parameters and drive project growth by implementing a dynamic, revenue-generating feature.',
-      challenges:
-        'Adapting to Grails and Groovy under time constraints and ensuring accurate data comparisons. Overcoming these hurdles demanded rapid skill acquisition, creativity, and effective problem-solving.',
-      results:
-        'The feature’s success not only delighted the client—leading to expansions and higher revenue—but also reinforced trust in the platform’s capabilities and elevated the company’s reputation for innovation.',
-      tags: [
-        Tag.Git,
-        Tag.Agile,
-        Tag.Highlight
-      ]
-    },
-    {
-      id: 'project-1',
-      title: 'Google Maps Integration for Sipag App',
-      imageUrl: 'assets/images/sipag-google-maps.png',
-      detailImageUrl: 'assets/images/sipag-delivery.webp',
-      description:
-        'Integrated Google Maps API into the Sipag app to enhance vendor registration by enabling precise geolocation. This feature improved accessibility for Cabal workers and delivery drivers, facilitating faster delivery of payment terminals and enhancing overall user experience.',
-      objective: 'To improve vendor registration by allowing precise location identification and enabling faster, more reliable delivery of card machines.',
-      challenges:
-        'Overcame initial inexperience with API integration and addressed the complexities of unreliable addresses and chaotic street layouts in Brazilian cities.',
-      results:
-        'Significantly improved operational efficiency by enabling precise pin placement on maps. Delivery drivers used Street View to locate vendors, reducing delays caused by address confirmation issues. Vendors received card machines faster, ensuring uninterrupted service and revenue generation for Cabal.',
-      tags: [
-        Tag.Java,
-        Tag.GoogleMapsAPI,
-        Tag.Highlight],
-    },
-    {
-      id: 'project-3',
-      title: 'Dijkstra’s Algorithm for Vancouver SkyTrain',
-      imageUrl: 'assets/images/train-station-algorithm.png',
-      detailImageUrl: 'assets/images/train-station-algorithm-detail.webp',
-      description:
-        'Utilized Dijkstra’s algorithm to implement a shortest-path solution for calculating travel distances between train stations on the Vancouver SkyTrain network. Integrated into the Scadacom platform, this feature provided real-time tracking of total distance traveled by individual trains, informing maintenance schedules and improving operational reliability.',
-      objective:
-        'To enhance asset reliability and streamline maintenance planning by accurately quantifying distance traveled, thus proactively guiding timely servicing and reducing the risk of unexpected downtime.',
-      challenges:
-        'Adapting a computationally intensive algorithm for real-time operations, ensuring integration within existing SCADA systems, and balancing efficiency with the complexity of large-scale transportation networks. Additionally, working within legacy code and conducting on-site testing with clients required strong communication and technical adaptability.',
-      results:
-        'Delivered a refined, fully integrated solution that became instrumental for Vancouver SkyTrain. The system facilitated data-driven maintenance decisions, likely increasing train availability and reliability. Post-launch, coordinated live testing and monitoring with clients in Vancouver ensured smooth adoption and sustained operational benefits.',
-      tags: [
-        Tag.Highlight
-      ]
-    },
-    {
       id: 'project-2',
       title: 'Intricate Team Leadership & Mentorship',
       imageUrl: 'assets/images/intricate-team.png',
@@ -129,6 +76,60 @@ export class ProjectService {
         Tag.DynamoDB,
         Tag.APIGateway,
         Tag.Serverless
+      ]
+    },
+    {
+      id: 'project-4',
+      title: 'Innovative File Comparison',
+      imageUrl: 'assets/images/tag-safety.png',
+      detailImageUrl: 'assets/images/tag-safety-detail.webp',
+      description:
+        'Developed a CSV file comparison feature for the TagSafety platform, enabling clients to visualize and reconcile updates against existing electrical safety data. Initially unfamiliar with Grails and Groovy, I leveraged AI-assisted learning to quickly adapt, delivering a streamlined, intuitive tool that surpassed the client’s expectations. Enthusiastic feedback prompted additional feature requests, bringing in new revenue opportunities, and earned me personal commendations from the CEO.',
+      objective:
+        'To simplify the client’s workflow for updating electrical safety parameters and drive project growth by implementing a dynamic, revenue-generating feature.',
+      challenges:
+        'Adapting to Grails and Groovy under time constraints and ensuring accurate data comparisons. Overcoming these hurdles demanded rapid skill acquisition, creativity, and effective problem-solving.',
+      results:
+        'The feature’s success not only delighted the client—leading to expansions and higher revenue—but also reinforced trust in the platform’s capabilities and elevated the company’s reputation for innovation.',
+      tags: [
+        Tag.Git,
+        Tag.Agile,
+        Tag.Highlight
+      ]
+    },
+    {
+      id: 'project-1',
+      title: 'Google Maps Integration for Sipag App',
+      imageUrl: 'assets/images/sipag-google-maps.png',
+      detailImageUrl: 'assets/images/sipag-delivery.webp',
+      description:
+        'Integrated Google Maps API into the Sipag app to enhance vendor registration by enabling precise geolocation. This feature improved accessibility for Cabal workers and delivery drivers, facilitating faster delivery of payment terminals and enhancing overall user experience.',
+      objective: 'To improve vendor registration by allowing precise location identification and enabling faster, more reliable delivery of card machines.',
+      challenges:
+        'Overcame initial inexperience with API integration and addressed the complexities of unreliable addresses and chaotic street layouts in Brazilian cities.',
+      results:
+        'Significantly improved operational efficiency by enabling precise pin placement on maps. Delivery drivers used Street View to locate vendors, reducing delays caused by address confirmation issues. Vendors received card machines faster, ensuring uninterrupted service and revenue generation for Cabal.',
+      tags: [
+        Tag.Java,
+        Tag.GoogleMapsAPI,
+        Tag.Highlight
+      ]
+    },
+    {
+      id: 'project-3',
+      title: 'Dijkstra’s Algorithm for Vancouver SkyTrain',
+      imageUrl: 'assets/images/train-station-algorithm.png',
+      detailImageUrl: 'assets/images/train-station-algorithm-detail.webp',
+      description:
+        'Utilized Dijkstra’s algorithm to implement a shortest-path solution for calculating travel distances between train stations on the Vancouver SkyTrain network. Integrated into the Scadacom platform, this feature provided real-time tracking of total distance traveled by individual trains, informing maintenance schedules and improving operational reliability.',
+      objective:
+        'To enhance asset reliability and streamline maintenance planning by accurately quantifying distance traveled, thus proactively guiding timely servicing and reducing the risk of unexpected downtime.',
+      challenges:
+        'Adapting a computationally intensive algorithm for real-time operations, ensuring integration within existing SCADA systems, and balancing efficiency with the complexity of large-scale transportation networks. Additionally, working within legacy code and conducting on-site testing with clients required strong communication and technical adaptability.',
+      results:
+        'Delivered a refined, fully integrated solution that became instrumental for Vancouver SkyTrain. The system facilitated data-driven maintenance decisions, likely increasing train availability and reliability. Post-launch, coordinated live testing and monitoring with clients in Vancouver ensured smooth adoption and sustained operational benefits.',
+      tags: [
+        Tag.Highlight
       ]
     }
   ];
